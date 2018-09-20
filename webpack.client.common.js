e02cfecab7f07f4d6f3fa92c4f90ed9cfb6b9e8d
Esko Ikkala <esko.ikkala@aalto.fi>
2018-09-20 12:02:05 +0300

    url-loader not required with react-bootsrap, uninstall

--- webpack.client.common.js
+++ webpack.client.common.js
@@ -50,10 +50,6 @@ module.exports = {
           'file-loader',
         ],
       },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
-      },
     ],
   },
   resolve: {
