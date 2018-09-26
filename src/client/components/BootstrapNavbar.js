c9e81d66c3374117ef5e3aec9dfed7962b8b1b83
esikkala <esko.ikkala@aalto.fi>
2018-09-26 08:58:07 +0300

    Update navbar

--- src/client/components/BootstrapNavbar.js
+++ src/client/components/BootstrapNavbar.js
@@ -15,7 +15,7 @@ const BootstrapNavbar = () => (
   >
     <Navbar.Header>
       <Navbar.Brand>
-        <a href="#brand" style={{padding: '0px 0px 10px 0px'}}>
+        <a href="http://biografiasampo.fi/" style={{padding: '0px 0px 10px 0px'}}>
           <span>
             <img width="90" height="50" src="img/logos/agricola.jpg" />
           </span>
@@ -24,16 +24,30 @@ const BootstrapNavbar = () => (
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
-      <Nav>
-        <NavDropdown eventKey={3} title="Näkymät" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}><span className="glyphicon glyphicon-search" aria-hidden="true"></span> Hae ja selaa</MenuItem>
-          <MenuItem eventKey={3.2}><span className="glyphicon glyphicon glyphicon-globe" aria-hidden="true"></span> Kartat</MenuItem>
-          <MenuItem eventKey={3.3}><span className="glyphicon glyphicon-stats" aria-hidden="true"></span> Tilastot</MenuItem>
-          <MenuItem eventKey={3.3}><span className="glyphicon glyphicon-link" aria-hidden="true"></span> Verkostoanalyysi</MenuItem>
-          <MenuItem eventKey={3.3}><span className="glyphicon glyphicon-retweet" aria-hidden="true"></span> Yhteyshaku</MenuItem>
-          <MenuItem eventKey={3.3}><span className="glyphicon glyphicon-book" aria-hidden="true"></span> Kielianalyysi</MenuItem>
-        </NavDropdown>
+      <Nav pullLeft>
+        <NavItem eventKey={1} href="http://biografiasampo.fi/haku">
+          <span className="glyphicon glyphicon-search" aria-hidden="true"></span> Haku
+        </NavItem>
+        <NavItem eventKey={1} href="http://biografiasampo.fi/kartat/tapahtumat">
+          <span className="glyphicon glyphicon-map-marker" aria-hidden="true"></span> Kartat
+        </NavItem>
+        <NavItem eventKey={1} href="#">
+          <span className="glyphicon glyphicon-globe" aria-hidden="true"></span> Paikat
+        </NavItem>
+        <NavItem eventKey={1} href="http://biografiasampo.fi/tilastot/palkit">
+          <span className="glyphicon glyphicon-stats" aria-hidden="true"></span> Tilastot
+        </NavItem>
+        <NavItem eventKey={1} href="http://biografiasampo.fi/verkostot">
+          <span className="glyphicon glyphicon-link" aria-hidden="true"></span> Verkostot
+        </NavItem>
+        <NavItem eventKey={1} href="http://biografiasampo.fi/yhteyshaku/">
+          <span className="glyphicon glyphicon-resize-small" aria-hidden="true"></span> Yhteyshaku
+        </NavItem>
+        <NavItem eventKey={1} href="http://biografiasampo.fi/kielianalyysi">
+          <span className="glyphicon glyphicon-book" aria-hidden="true"></span> Kielianalyysi
+        </NavItem>
       </Nav>
+
       <Nav pullRight>
         <NavItem eventKey={1} href="#">
           Palaute
